f94032db1cbef152b17f4dd2a90ac90db5281596
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2022-08-08 17:58:08 -0600

    Configuracion del archivo main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_delivery_udemy/src/login/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App Flutter',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {'login': (BuildContext context) => LoginPage()},
+    );
+  }
+}
